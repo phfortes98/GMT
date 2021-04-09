a49abae330e4ea03bead07eff4ac1c71d9172e98
phfortes98 <pfortes05@gmail.com>
2021-04-09 11:19:04 -0400

    edited new node layout

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -11,9 +11,20 @@ import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
     ]
   });
   
+  const NewNode = ({}) => (
+    <div className='button blue' style={{ fontSize: '0.5rem',textAlign: 'left', padding: '4px', width: '70px', height: '40px'}}>
+      <a>
+      <div role="button">
+        <label>Form: </label><input style={{width: '25px', height: '12px'}} type='text'></input> <button className="submitf" icon='plus'>+</button> <br></br>
+        
+        <label>Func: </label><input style={{width: '25px', height: '12px'}}type='text'></input><button className="submitf" icon='plus'>+</button> 
+      </div>
+      </a>
+    </div>
+);
   
-  const CustomRender = ({content}) => (
-      <div className='button' style={{width: '60px', fontSize: '0.6rem', textAlign: 'center'}}>
+  const BaseNode = ({content}) => (
+      <div className='button' style={{width: '70px', fontSize: '0.6rem', textAlign: 'center'}}>
         <a>
         <div role="button">
           {content}
@@ -34,9 +45,9 @@ import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
         wordNodes.forEach((word,index)=>{
           const node={
             id: `node-${index}`,
-            coordinates: [80+80*index, 280],
+            coordinates: [80+80*index, 320],
             content: word,
-            render: CustomRender,//()=><div style={{backgroundColor: 'lightblue',borderColor: 'black', width: '70px', borderRadius: '10px', padding: '8px'}} onClick={()=>clickNode(word)}>{word}</div>
+            render: BaseNode,//()=><div style={{backgroundColor: 'lightblue',borderColor: 'black', width: '70px', borderRadius: '10px', padding: '8px'}} onClick={()=>clickNode(word)}>{word}</div>
           };
           addNode(node);
         });
@@ -57,7 +68,7 @@ import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
            schema.nodes[schema.nodes.length - 1].coordinates[0] + 100,
            schema.nodes[schema.nodes.length - 1].coordinates[1],
          ],
-         render: CustomRender,
+         render: NewNode,
          data: {onClick: deleteNodeFromSchema},
          inputs: [{ id: `port-${Math.random()}`}],
          outputs: [{ id: `port-${Math.random()}`}],
@@ -68,8 +79,8 @@ import Diagram, { createSchema, useSchema } from 'beautiful-react-diagrams';
   
     return (
       <div style={{ height: '27rem' }}>
-        <Button color="primary" icon="plus" onClick={addNewNode}>Add new node</Button>
-        <Button color="secondary" icon="minus">Delete Node</Button>
+        <Button color="primary" icon="plus" style={{fontSize: '12px'}} onClick={addNewNode}>Add new node</Button>
+        <Button color="secondary" icon="minus" style={{fontSize: '12px'}}>Delete Node</Button>
         <Diagram style={{height: '100%', overflow: 'scroll'}} schema={schema} onChange={onChange} />
       </div>
     );
